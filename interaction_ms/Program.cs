bf480a487702ac91fcc7e1a751483463526432a0
Nicolas Viveros <nviverosb@unal.edu.co>
2019-11-18 10:06:40 -0500

    Port change successful

--- interaction_ms/Program.cs
+++ interaction_ms/Program.cs
@@ -11,8 +11,8 @@ using Microsoft.Extensions.Logging;
 namespace interaction_ms {
     public class Program {
         public static void Main(string[] args) {
-            // var defaultAddress = Environment.GetEnvironmentVariable("DEFAULT_ADDRESS");
-            var defaultAddress = "https://0.0.0.0:4005/";
+            var defaultAddress = Environment.GetEnvironmentVariable("DEFAULT_ADDRESS");
+            // var defaultAddress = "http://0.0.0.0:4005/";
             CreateWebHostBuilder(args, defaultAddress).Build().Run();
         }
 
